3cc4fe077e133718f24b54acd4e71fb383d8eb4b
Jorge <jorge.alfmur@gmail.com>
2017-05-26 00:41:07 -0500

    contructor y ngOnInit, metodo propio de todos los componentes de angular

--- src/app/fruta/fruta.component.ts
+++ src/app/fruta/fruta.component.ts
@@ -8,4 +8,21 @@ export class FrutaComponent {
   nombre_componente:string = 'Componente de fruta'
   listado_frutas:string = 'Naranja, Manzana, Pera y Sandia'
   trabajos:Array<any> = ['Informatico', 'Diseñador', 24]
+
+  //constructor
+  constructor(){
+  	console.log(this.trabajos)
+  	//this.holaMundo()
+  }
+
+  //llamar metodos sin tener que declararlos en constructor
+  ngOnInit(){
+  	//this.holaMundo()
+  }
+
+  //metodo
+  holaMundo(){
+  	alert('Hola Mundo')
+  }
+
 }

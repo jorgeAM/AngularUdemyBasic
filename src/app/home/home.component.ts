da6b5df9610f0823b281c3322700aa975520ffe2
Jorge <jorge.alfmur@gmail.com>
2017-05-28 00:00:05 -0500

    agregamos prendas y las eliminamos con services

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,11 +9,23 @@ import { RopaService } from '../services/ropa.service';
 })
 export class HomeComponent {
   titulo = 'Pagina Principal';
+  listado_ropa:Array<string>
+  prenda_a_guardar:string 
   //esto para no usar new -> NO ES RECOMENDABLE
   constructor(private ropaService: RopaService) { }
 
   ngOnInit(){
-  	console.log(this.ropaService.prueba('Estoy calato'))
+  	this.listado_ropa = this.ropaService.getRopa()
+  	console.log(this.listado_ropa)
+  }
+
+  guardarPrenda(){
+  	this.ropaService.addRopa(this.prenda_a_guardar)
+  	this.prenda_a_guardar = null
+  }
+
+  eliminarPrenda(n:number){
+  	this.ropaService.deleteRopa(n)
   }
 
 }
--- src/app/services/ropa.service.ts
+++ src/app/services/ropa.service.ts
@@ -3,8 +3,24 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class RopaService {
 	nombre_prenda:string = 'Pantalones pitillo'
-	
+	coleccion_ropa = ['jeans azul', 'polo blanco']
+
 	prueba(nombre_prenda:string){
 		return nombre_prenda
+	}
+
+	addRopa(coleccion_ropa:string):Array<string>{
+		//agregamos al array
+		this.coleccion_ropa.push(coleccion_ropa)
+		return this.getRopa()
+	}
+
+	deleteRopa(index:number){
+		this.coleccion_ropa.splice(index, 1)
+		return this.getRopa()
+	}
+
+	getRopa(){
+		return this.coleccion_ropa;
 	}
 }
